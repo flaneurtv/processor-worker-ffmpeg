72a03bd35b3f1423a1f197464edafb31e9648489
Markus Kienast <mark@trickkiste.at>
2017-10-12 14:06:02 +0200

    updated test routine

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
 FROM jrottenberg/ffmpeg:3.3-alpine as ffmpeg
 
-FROM node:8-alpine
+FROM golang:1.9-alpine as builder
 COPY --from=ffmpeg /usr/local/ /usr/local/
-RUN apk add --no-cache --update ca-certificates libcrypto1.0 libssl1.0 libgomp expat
+RUN apk add --no-cache --update ca-certificates libcrypto1.0 libssl1.0 libgomp expat libgcc libstdc++
+COPY . /go/src/github.com/flaneurtv/worker-ffmpeg/
 
-WORKDIR /srv
+RUN cd /go/src/github.com/flaneurtv/worker-ffmpeg \
+    && apk add --no-cache \
+        git \
+        gettext \
+        jq \
+    && go get -d -v ./... \
+    && GOOS=linux GOARCH=amd64 go build -o "/go/bin/worker-ffmpeg" ./main.go 
+
+WORKDIR /go
+
 CMD ash
--- test.sh
+++ test.sh
@@ -1,12 +1,30 @@
 #/!bin/sh
+### USAGE ###
+# ./test.sh test.json [in]
+# If you want to debug the input messages and want to see them nicely formatted
+# just add "in" to the end of the test command:  
+# ./test.sh test.json in
+#
+# To see the output of your service-core, just run the test command like this:
+# ./test.sh test.json
+
+export SERVICE_UUID=WORKER00-1285-4E4C-A44E-AAAABBBB0000 
+export SERVICE_NAME=micro-worker-ffmpeg 
+export SERVICE_HOST=worker00 
+export NAMESPACE_LISTENER=flaneur 
+export NAMESPACE_PUBLISHER=flaneur
+
 TESTFILE=$1
-PROCESSOR="${@:2}"
+IN=$2
+case $IN in
+  in) PROCESSOR="cat -" ;;
+  *) PROCESSOR="/go/bin/worker-ffmpeg" ;;
+esac
 
 while read line
 do
-  if [[ $line == sleep* ]] ; then
-    sleep `echo $line | tr -s ' ' | cut -d ' ' -f 2`
-  else
-    echo $line | CREATED_AT=`date -u +"%FT%T.000Z"` envsubst
-  fi
-done <$TESTFILE | SERVICE_UUID=WORKER00-1285-4E4C-A44E-AAAABBBB0000 SERVICE_NAME=micro-worker-ffmpeg SERVICE_HOST=worker NAMESPACE_LISTENER=flaneur NAMESPACE_PUBLISHER=flaneur $PROCESSOR
+  case $line in
+    sleep*) sleep `echo $line | tr -s ' ' | cut -d ' ' -f 2` ;;
+    *) echo $line | CREATED_AT=`date -u +"%FT%T.000Z"` envsubst ;;
+  esac
+done < $TESTFILE | $PROCESSOR | jq '.'
